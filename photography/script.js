09a8ab59c5dfb2516c14f7fe9bd8a8c748c8aa8e
KDSDKsk <surangadarshana68@gmail.com>
2023-09-08 22:27:10 +0530

    Updated

--- photography/script.js
+++ photography/script.js
@@ -13,3 +13,22 @@ var swiper = new Swiper(".mySwiper", {
       slideShadows: true,
     },
   });
+
+const tglbtn = document.querySelector('.toggle_btn')
+const ddmenu = document.querySelector('.dropdown_menu')
+const subMenu = document.querySelector('.sub_menu')
+const proSub = document.querySelector('.project_sub')
+
+proSub.onclick = function () {
+  subMenu.classList.toggle('animation')
+};
+
+tglbtn.onclick = function () {
+  ddmenu.classList.toggle('open')
+};
+
+
+tglbtn.addEventListener('click', () => {
+  tglbtn.classList.toggle('active')
+});
+
